f8ab595d5865713223f0f434248837f39f666d1c
Bartlomiej Gotfryd <barto.gotfryd33@gmail.com>
2025-06-27 18:45:18 +0200

    Subfolder for header component created. New "user" component created and displayed on main page. Updated root component css.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header.component';
+import { HeaderComponent } from './header/header.component';
+import { User } from "./user/user.component";
+
 
 @Component({
   selector: 'app-root-comp',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, User],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
